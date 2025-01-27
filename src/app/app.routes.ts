7fa063bd8fc23199d8d901459e779d5d645c0163
toniramss <toni.rams.sebastia@gmail.com>
2025-01-27 16:03:28 +0100

    Agregar componente de inicio de sesión y actualizar configuración del proyecto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { AppComponent } from './app.component'; // Este será tu componente para el index
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: AppComponent }, // Ruta principal (index)
+  { path: 'login', component: LoginComponent }, // Ruta para /login
+];
