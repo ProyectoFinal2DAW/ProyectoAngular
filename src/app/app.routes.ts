65eb8e1bac5752f6a9f5218baf1e7cfb13766b6e
toniramss <toni.rams.sebastia@gmail.com>
2025-01-27 17:05:38 +0100

    Agregar componente de inicio de sesión y personalizar estilos y temas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,9 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { AppComponent } from './app.component'; // Este será tu componente para el index
+import { Login1Component } from './login1/login1.component';
 
 export const routes: Routes = [
   { path: '', component: AppComponent }, // Ruta principal (index)
-  { path: 'login', component: LoginComponent }, // Ruta para /login
+  { path: 'login', component: Login1Component }, // Ruta para /login
 ];
