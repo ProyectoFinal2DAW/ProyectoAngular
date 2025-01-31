1f4009a9abebc80f67f6be9cf8ea32bd58edb227
toniramss <toni.rams.sebastia@gmail.com>
2025-01-31 16:40:23 +0100

    Agregar componente ItemExperiment y su integración en la vista de Experimentos

---
+++ src/app/components/item-experiment/item-experiment.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-item-experiment',
+  imports: [],
+  templateUrl: './item-experiment.component.html',
+  styleUrl: './item-experiment.component.css'
+})
+export class ItemExperimentComponent {
+
+}
