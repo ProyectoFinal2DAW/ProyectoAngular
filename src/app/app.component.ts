875c1f1abe3e581a77a3f1aab640d77fa04e54c5
toniramss <toni.rams.sebastia@gmail.com>
2025-02-14 17:04:07 +0100

    Reestructurar componente Footer: mover a nueva ubicación y agregar archivos de prueba

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from "./components/login/login.component";
 import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from "./footer/footer.component";
+import { FooterComponent } from "./components/footer/footer.component";
 
 
 
