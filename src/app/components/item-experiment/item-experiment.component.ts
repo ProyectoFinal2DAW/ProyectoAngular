ff25aa1905d5c99dea2c2f7c5aff13181afc7579
toniramss <toni.rams.sebastia@gmail.com>
2025-02-19 17:16:30 +0100

    Actualizar diseño del componente de experimentos: mejorar la estructura HTML, agregar contador de experimentos y ajustar estilos.

--- src/app/components/item-experiment/item-experiment.component.ts
+++ src/app/components/item-experiment/item-experiment.component.ts
@@ -10,4 +10,6 @@ import { Component, Input } from '@angular/core';
 export class ItemExperimentComponent {
   @Input() nombreExperimento: string = "";
   @Input() imagenExperimento: string = "";
+  @Input() descripcionExperimento: string = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
+  @Input() contadorExperimento: number = 0;
 }
