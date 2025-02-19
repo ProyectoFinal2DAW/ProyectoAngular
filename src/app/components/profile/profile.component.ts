d7723c45a84284ba5a0c62e5785f7b0dcc17c240
CRI88 <christianrieigl@monlau.com>
2025-02-19 16:16:21 +0100

    Refactor profile component: update layout, remove user profile component, and enhance styles

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../header/header.component";
-import { UserProfileComponent } from "../user-profile/user-profile.component";
+
+import {MatInputModule} from '@angular/material/input';
+
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-profile',
-  imports: [HeaderComponent, UserProfileComponent],
+  imports: [HeaderComponent, MatInputModule, MatButtonModule],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
