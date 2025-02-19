c06168ec1155376435ca8d4d24894885805a21b4
CRI88 <christianrieigl@monlau.com>
2025-02-19 17:45:36 +0100

    Add logout icon and enhance profile editing functionality with save and cancel buttons

--- src/app/components/login1/login1.component.ts
+++ src/app/components/login1/login1.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login1',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './login1.component.html',
   styleUrl: './login1.component.css'
 })
--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from "../header/header.component";
 
 import {MatInputModule} from '@angular/material/input';
 
@@ -7,10 +6,24 @@ import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-profile',
-  imports: [HeaderComponent, MatInputModule, MatButtonModule],
+  imports: [MatInputModule, MatButtonModule],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
 export class ProfileComponent {
+  editando = false;
 
+  activarEdicion() {
+    this.editando = true;
+  }
+
+  guardar() {
+    console.log('Guardado');
+    this.editando = false;
+  }
+
+  cancelar() {
+    console.log('Cancelado');
+    this.editando = false;
+  }
 }
