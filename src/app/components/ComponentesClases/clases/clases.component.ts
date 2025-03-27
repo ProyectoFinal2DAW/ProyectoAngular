a939ce5564b59fa5ee9cb42d26cfd848fe9c9183
toniramss <toni.rams.sebastia@gmail.com>
2025-03-27 16:26:23 +0100

    Agregar cuadro de diálogo para añadir clases: implementar formulario y lógica de envío

--- src/app/components/ComponentesClases/clases/clases.component.ts
+++ src/app/components/ComponentesClases/clases/clases.component.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { ItemClasroomComponent } from "../item-clasroom/item-clasroom.component";
 import { Class } from '../../../../interfaces/class';
 import { CuadroDialogoAddClassComponent } from "../cuadro-dialogo-add-class/cuadro-dialogo-add-class.component";
 import { getClasses } from '../../../DBManagement/DBManagement';
+import { MatDialog, MatDialogModule, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Dialog } from '@angular/cdk/dialog';
+import { DialogContentAddClass } from './CuadrosDeDialogo/AddClass/dialog-content-add-class';
+
 
 @Component({
   selector: 'app-clases',
@@ -15,7 +19,7 @@ export class ClasesComponent {
 
   listClasses: Class[] = [];
 
-  crearClaseVisible: Boolean = false;
+  /* crearClaseVisible: Boolean = false;
 
 
   showPanelAddClass() {
@@ -23,10 +27,25 @@ export class ClasesComponent {
     this.crearClaseVisible = !this.crearClaseVisible;
     console.log("Show pop up add class");  
 
-  }
+  } */
 
   async ngOnInit() {
     this.listClasses = await getClasses();
   }
+
+
+    //---------------------Cuadro de diálogo Add video-------------------------------
+    readonly dialogAddClass = inject(MatDialog);
+  
+    openDialogAddClass() {
+      const dialogRefAddClass = this.dialogAddClass.open(DialogContentAddClass, {
+  
+      });
+  
+      dialogRefAddClass.afterClosed().subscribe(result => {
+        console.log(`Dialog result: ${result}`);
+      });
+    }
+    //---------------------------------------------------------------------
 
 }
