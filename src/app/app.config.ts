0cfc0167f1ac499ae4b699a8d0831b6d294007ba
toniramss <toni.rams.sebastia@gmail.com>
2025-05-20 19:52:18 +0200

    Agregar configuración de MSAL para autenticación y autorización

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,67 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, APP_INITIALIZER } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { 
+  MSAL_INSTANCE, 
+  MsalService, 
+  MsalInterceptor, 
+  MsalGuard, 
+  MsalBroadcastService 
+} from '@azure/msal-angular';
+import { 
+  InteractionType, 
+  PublicClientApplication, 
+  BrowserCacheLocation 
+} from '@azure/msal-browser';
+
+// MSAL factory function
+export function MSALInstanceFactory() {
+  return new PublicClientApplication({
+    auth: {
+      clientId: '16176e00-0931-40e6-bca3-9c181a76f11f',
+      authority: 'https://login.microsoftonline.com/481fa64d-78ed-4ec6-ba40-c327c2b194f6',
+      redirectUri: 'http://localhost:8000/callback'
+    },
+    cache: {
+      cacheLocation: BrowserCacheLocation.LocalStorage,
+      storeAuthStateInCookie: false,
+    }
+  });
+}
+
+export function MSALInitializerFactory(msalInstance: PublicClientApplication) {
+  return () => msalInstance.initialize();
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes), 
     provideClientHydration(withEventReplay()), 
-    provideAnimationsAsync()
+    provideAnimationsAsync(),
+    provideHttpClient(withInterceptorsFromDi()),
+    
+    // MSAL providers
+    {
+      provide: MSAL_INSTANCE,
+      useFactory: MSALInstanceFactory
+    },
+    {
+      provide: APP_INITIALIZER,
+      useFactory: MSALInitializerFactory,
+      deps: [MSAL_INSTANCE],
+      multi: true
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: MsalInterceptor,
+      multi: true
+    },
+    MsalService,
+    MsalGuard,
+    MsalBroadcastService
   ]
 };
