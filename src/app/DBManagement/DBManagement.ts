894e6a6b4013fccc7ccb5be6d370bc58dfa781ca
CRI88 <christianrieigl@monlau.com>
2025-05-20 16:53:07 +0200

    Revert base API URL to localhost for local development

--- src/app/DBManagement/DBManagement.ts
+++ src/app/DBManagement/DBManagement.ts
@@ -19,7 +19,7 @@ import { NewExperimento } from "../../interfaces/newExperimento";
 import exp from "constants";
 import { ExperimentData } from "../../interfaces/experimentData";
 
-const baseApiUrl = "http://79.154.117.109:6956/";
+const baseApiUrl = "http://localhost:8005/";
 
 /*------------------ Subir archivos -----------------*/
 
